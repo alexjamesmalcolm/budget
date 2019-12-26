b093ad9de17c94437db4df682e7df90edc3d2329
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2019-12-26 15:39:21 -0500

    Added labels to ProgressBar

--- scripts/components/Goal.js
+++ scripts/components/Goal.js
@@ -7,8 +7,16 @@ const GoalTemplate = (goal = new Goal(), removeGoal = () => {}) => {
   return html`
     <section>
       <h2>${goal.name}</h2>
-      <p>Percent done: ${round(goal.percentDone)}%</p>
-      ${ProgressBar(goal.percentDone)}
+      <div class="progress-bar-grid">
+        <p>${goal.startDate.toLocaleDateString()}</p>
+        ${ProgressBar(
+          goal.percentDone,
+          `$${goal.currentAmount}`,
+          `${round(goal.percentDone)}%`
+        )}
+        <p>$${goal.desiredAmount}</p>
+        <p>${goal.endDate.toLocaleDateString()}</p>
+      </div>
       <p>At a rate of $${round(goal.idealAmountPerFortnight)} every 2 weeks.</p>
       <p>
         You have ${Math.floor(goal.daysBetweenCurrentAndEnd)} days left to meet
--- scripts/components/ProgressBar.js
+++ scripts/components/ProgressBar.js
@@ -1,9 +1,12 @@
 import { html } from "../dependencies/lit-html.js";
 import round from "../utils/round.js";
 
-const ProgressBar = percent => html`
+const ProgressBar = (percent, leftLabel, rightLabel) => html`
   <div class="progress-bar container">
-    <div class="progress-bar filler" style="width: ${round(percent)}%;"></div>
+    <div class="progress-bar filler" style="width: ${round(percent)}%;">
+      <p class="progress-bar label left-label">${leftLabel}</p>
+    </div>
+    <p class="progress-bar label right-label">${rightLabel}</p>
   </div>
 `;
 
